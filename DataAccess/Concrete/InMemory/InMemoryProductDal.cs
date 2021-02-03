b780c0fa750dc106ab15fd3da739b173fe5e02ed
Mübin Güneş <mubin.gunes@pulur.com.tr>
2021-02-03 22:26:35 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,75 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+                new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15 },
+                new Product{ProductId=2,CategoryId=1,ProductName="Tabak",UnitPrice=10,UnitsInStock=150 },
+                new Product{ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice=500,UnitsInStock=10 },
+                new Product{ProductId=4,CategoryId=2,ProductName="Kamera",UnitPrice=1500,UnitsInStock=20 },
+                new Product{ProductId=5,CategoryId=2,ProductName="Klavye",UnitPrice=50,UnitsInStock=30 },
+
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //Product productToDelete=null;
+            //foreach (var p in _products)
+            //{
+            //    if (product.ProductId==p.ProductId)
+            //    {
+            //        productToDelete = p;
+            //    }
+            //}
+
+            Product productToDelete = _products.SingleOrDefault(p=>p.ProductId==product.ProductId);
+            _products.Remove(productToDelete);
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            List<Product> getAllByCategoryId = _products.Where(p => p.CategoryId == categoryId).ToList();
+            return getAllByCategoryId;
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate= _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+    }
+}
