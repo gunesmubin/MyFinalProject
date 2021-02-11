2fbed06e49b50bf9737cbef87761e574dbd49fcc
Mübin Güneş <mubin.gunes@pulur.com.tr>
2021-02-11 10:46:26 +0300

    10 Subat Calismasi. Result ,islemleri

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenceTime = "Sistem Bakım Zamanı";
+        public static string ProductListed = "Ürünler Listelendi";
+
+    }
+}
