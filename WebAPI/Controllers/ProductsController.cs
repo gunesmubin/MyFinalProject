3e4850718bcf24a9da9a4d2e057bb22e66eeb951
Mübin Güneş <mubin.gunes@pulur.com.tr>
2021-02-13 22:47:21 +0300

    WebAPI Projesi Eklendi

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loosely Couplet
+        //Naming Convention
+        //IoC Container -- Inversion of Control
+        IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
