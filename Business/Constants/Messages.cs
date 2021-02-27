6a21957bd371af185f1639ba108306f758f4d89b
Mübin Güneş <mubin.gunes@pulur.com.tr>
2021-02-28 01:35:57 +0300

    Kullanıcı Yetkilendirme Süreci Eklendi

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,5 +16,13 @@ namespace Business.Constants
         public static string ProductNameAlreadyExists = "Ürün Adı Zaten Var";
 
         public static string CategoryLimitExceded = "Kategori Limiti Aşıldı";
+
+        public static string AuthorizationDenied = "AuthorizationDenied";
+        public static string UserRegistered= "UserRegistered";
+        public static string UserNotFound= "UserNotFound";
+        public static string PasswordError= "PasswordError";
+        public static string SuccessfulLogin= "SuccessfulLogin";
+        public static string UserAlreadyExists= "UserAlreadyExists";
+        public static string AccessTokenCreated= "AccessTokenCreated";
     }
 }
